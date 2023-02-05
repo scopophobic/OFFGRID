23818e87238a03afa4731b40755e70d1d1049b8f
Adithyan Madhu <66241061+scopophobic@users.noreply.github.com>
2023-02-05 10:42:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,102 @@
-// // const html_code = document.querySelector('.html-code textarea');
-// // const css_code = document.querySelector('.css-code textarea');
-// // const js_code = document.querySelector('.js-code textarea');
-// var html_code = CodeMirror.fromTextArea(document.getElementById("html-code textarea"),{
-//     theme: "default",
-//     mode: "HTML",
-//     linenumber: true
-// });
-// console.log(html_code);
-// var css_code = CodeMirror.fromTextArea(document.getElementById(".css-code textarea"),{
-//     theme: "default",
-//     mode: "CSS",
-//     linenumber: true
-// });
-// var js_code = CodeMirror.fromTextArea(document.getElementById(".js-code textarea"),{
-//     theme: "default",
-//     mode: "javascript",
-//     linenumber: true
-// });
-// const result = document.querySelector('#result');
 
-// function run() {
-//     //Storing Data In Local Storage
-//     localStorage.setItem('html_code', html_code.value);
-//     localStorage.setItem('css_code', css_code.value);
-//     localStorage.setItem('js_code', js_code.value);
+const html_code = CodeMirror.fromTextArea(
+    document.querySelector("#html-code"),
+    {
+      theme: "3024-night",
+      mode: "xml",
+      background: "transparent",
+    }
+  );
+  const css_code = CodeMirror.fromTextArea(
+    document.querySelector("#css-code"),
+    {
+        theme: "3024-night",
+      mode: "css",
+    }
+  );
+  const js_code = CodeMirror.fromTextArea(
+    document.querySelector("#js-code"),
+    {
+        theme: "3024-night",
+      mode: "javascript",
+    }
+  );
+  const result = document.querySelector("#result");
+
+  function run() {
+    //Storing Data In Local Storage
+    localStorage.setItem("html_code", html_code.getValue());
+    localStorage.setItem("css_code", css_code.getValue());
+    localStorage.setItem("js_code", js_code.getValue());
+
+    result.contentDocument.body.innerHTML =
+      `<style> ${localStorage.css_code}</style>` + localStorage.html_code;
+    result.contentWindow.eval(localStorage.js_code);
+  }
+
+  html_code.on("change", () => run());
+  css_code.on("change", () => run());
+  js_code.on("change", () => run());
+
+  html_code.setValue(localStorage.html_code || "");
+  css_code.setValue(localStorage.css_code || "");
+  js_code.setValue(localStorage.js_code || "");
+
+//----------------------------------------------------------------
 
 
-//     result.contentDocument.body.innerHTML = `<style> ${localStorage.css_code}</style>` + localStorage.html_code;
-//     result.contentWindow.eval(localStorage.js_code);
-// }
+  var textEditor = document.getElementById("##css-code");
+var generatedText = document.getElementById("generated-text");
+var apiToken = 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;
 
-// html_code.onkeyup = () => run();
-// css_code.onkeyup = () => run();
-// js_code.onkeyup = () => run();
+// Function to pick the line automatically after it is commented and enclosed within our own unique syntax
+function pickLine() {
+  var lines = textEditor.value.split("\n");
+  for (var i = 0; i < lines.length; i++) {
+    if (lines[i].startsWith("##")) {
+      // Use Open AI API to generate text based on the prompt
+      var prompt = lines[i].slice(20);
+      var request = new XMLHttpRequest();
+      request.open("POST", "https://api.openai.com/v1/engines/text-davinci/jobs");
+      request.setRequestHeader("Content-Type", "application/json");
+      request.setRequestHeader("Authorization", "Bearer " + apiToken);
+
+      request.onreadystatechange = function() {
+        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+          var response = JSON.parse(this.responseText);
+          generatedText.innerHTML = "Generated Text based on the prompt: " + response.choices[0].text;
+        }
+      };
+
+      request.send(JSON.stringify({
+        prompt: prompt,
+        max_tokens: 100,
+        n: 1,
+        temperature: 0.5
+      }));
+      break;
+    }
+  }
+}
+
+// Check for syntax and prompt every few seconds
+setInterval(function() {
+  pickLine();
+}, 3000);
 
 
+console.log(generatedText);
 
-// html_code.value = localStorage.html_code;
-// css_code.value = localStorage.css_code;
-// js_code.value = localStorage.js_code;
+
+//-------------------
+window.onload = function() {
+    window.editor = CodeMirror.fromTextArea(code, {
+        mode: "javascript",
+        lineNumbers: true,
+        lineWrapping: true,
+        foldGutter: {
+            rangeFinder: new CodeMirror.fold.combine(CodeMirror.fold.brace, CodeMirror.fold.comment)
+        },
+        gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"]
+    });
+};
